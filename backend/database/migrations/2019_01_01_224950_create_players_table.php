527b38f05abb162e044680a821a69236953f21b5
marian0 <marianosantafe@gmail.com>
2019-01-01 20:12:19 -0300

    Migrations for teams and players tables

---
+++ backend/database/migrations/2019_01_01_224950_create_players_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('first_name');
+            $table->string('last_name');
+
+            $table->unsignedInteger('team_id')->nullable();
+            $table->foreign('team_id')->references('id')->on('teams');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
